8adb0a69cf5fb075c841a250cd05f45c29defb2b
OleksiiFurman <231003@student.pwr.edu.pl>
2016-05-20 15:37:39 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-//
-//  main.cpp
-//  LifeSimulator
-//
-//  Created by Alexey Furman on 19.05.16.
-//  Copyright © 2016 Alexey Furman. All rights reserved.
-//
 #include "Person.hpp"
 #include <iostream>
 
