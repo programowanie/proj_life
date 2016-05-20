4f9ec7f571b4a8f6ddc82a0c22707715102e9c36
OleksiiFurman <231003@student.pwr.edu.pl>
2016-05-20 15:38:33 +0200

    Update PartsOfLife.hpp

--- PartsOfLife.hpp
+++ PartsOfLife.hpp
@@ -1,11 +1,3 @@
-//
-//  PartsOfLife.hpp
-//  LifeSimulator
-//
-//  Created by Alexey Furman on 19.05.16.
-//  Copyright © 2016 Alexey Furman. All rights reserved.
-//
-
 #ifndef PartsOfLife_hpp
 #define PartsOfLife_hpp
 
